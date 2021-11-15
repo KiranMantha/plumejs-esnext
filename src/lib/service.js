031dd6b614b15fc1995c6d7085f40f2d48ce2e28
KiranMantha <kiranv.mantha@gmail.com>
2021-11-15 18:04:16 +0530

    refactored service implementation

--- src/lib/service.js
+++ src/lib/service.js
@@ -1,13 +1,20 @@
-import { Injector } from './injector';
-import { instantiate } from './instantiate';
+import { Injector } from "./injector";
+import { instantiate } from "./instantiate";
+
+const SERVICE_OPTIONS_DEFAULTS = {
+  name: "",
+  deps: [],
+};
 
 const Service = (options = {}, klass) => {
-  options.deps = options.deps || [];
+  options = { ...SERVICE_OPTIONS_DEFAULTS, ...options };
   if (options.name && klass) {
     const instance = instantiate(klass, options.deps);
     Injector.register(options.name, instance);
   } else {
-    throw 'error: Requires name in service options object and constructor to define service';
+    throw new Error(
+      "error: Requires name in service options object and constructor to define service"
+    );
   }
 };
 
