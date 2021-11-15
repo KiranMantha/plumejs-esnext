a6fbd5dd91f00f96080d3e7031d81d5f03284a04
KiranMantha <kiranv.mantha@gmail.com>
2021-11-15 17:52:02 +0530

    new build with updated useFormFields hook

--- dist/494.chunk.js
+++ dist/494.chunk.js
@@ -1,14 +1,14 @@
-"use strict";(self.webpackChunkplumejs_esnext=self.webpackChunkplumejs_esnext||[]).push([[494],{494:(e,t,s)=>{s.r(t);var a=s(746),i=s(669),l=s.n(i);(0,a.wA)({selector:"app-items",deps:["Renderer"]},class{constructor(e){this.sheetFormFields=void 0,this.changeHandler=void 0,this.resetForm=void 0,this.apiUrl="https://sheet.best/api/sheets/d406eddb-4e35-4496-a526-34fb27c763e4",this.table=void 0,this.personsList=[]}beforeMount(){const{formFields:e,createChangeHandler:t,resetFormFields:s}=(0,a.uA)({name:"",age:"",salary:""});this.sheetFormFields=e,this.changeHandler=t,this.resetForm=s}mount(){this.getData()}submitForm(e){var t=this;e.preventDefault(),l().post(this.apiUrl,this.sheetFormFields).then((function(e){t.personsList.push(...e.data),t.resetForm(),t.renderer.update()}))}getData(){var e=this;l().get(this.apiUrl).then((function(t){e.personsList=[...t.data],e.renderer.update()}))}render(){var e=this;return a.dy`
-        <form
-          onsubmit=${function(t){e.submitForm(t)}}
-        >
+"use strict";(self.webpackChunkplumejs_esnext=self.webpackChunkplumejs_esnext||[]).push([[494],{494:(e,t,s)=>{s.r(t);var a=s(746),i=s(669),l=s.n(i);(0,a.wA)({selector:"app-items",deps:["Renderer"]},class{constructor(e){this.sheetFormFields=void 0,this.changeHandler=void 0,this.resetForm=void 0,this.apiUrl="https://sheet.best/api/sheets/d406eddb-4e35-4496-a526-34fb27c763e4",this.table=void 0,this.personsList=[]}beforeMount(){[this.sheetFormFields,this.changeHandler,this.resetForm]=(0,a.uA)({name:"",age:"",salary:""})}mount(){this.getData()}submitForm(e){var t=this;e.preventDefault(),l().post(this.apiUrl,this.sheetFormFields).then((function(e){t.personsList.push(...e.data),t.resetForm(),t.renderer.update()}))}getData(){var e=this;l().get(this.apiUrl).then((function(t){e.personsList=[...t.data],e.renderer.update()}))}render(){var e=this;return a.dy`
+      <form
+        onsubmit=${function(t){e.submitForm(t)}}
+      >
         <div class="field">
           <label class="label" for="exampleInputEmail1">Name</label>
           <div class="control">
             <input
               type="text"
               class="input"
-              id='name'
+              id="name"
               value=${this.sheetFormFields.name}
               onchange=${this.changeHandler("name")}
             />
@@ -20,7 +20,7 @@
             <input
               type="text"
               class="input"
-              id='age'
+              id="age"
               value=${this.sheetFormFields.age}
               onchange=${this.changeHandler("age")}
             />
@@ -32,7 +32,7 @@
             <input
               type="text"
               class="input"
-              id='salary'
+              id="salary"
               value=${this.sheetFormFields.salary}
               onchange=${this.changeHandler("salary")}
             />
@@ -40,26 +40,28 @@
         </div>
         <div class="field">
           <div class="control">
-            <button class="button  is-info is-light" type="submit">Submit</button>
+            <button class="button  is-info is-light" type="submit">
+              Submit
+            </button>
           </div>
         </div>
       </form>
       <table class="table is-hoverable">
         <thead>
-            <tr>
-              <td>Name</td>
-              <td>Age</td>
-              <td>Salary</td>
-            </tr>
+          <tr>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Salary</td>
+          </tr>
         </thead>
         <tbody>
-        ${this.personsList.map((function(e){return a.dy`
-            <tr>
-              <td>${e.name}</td>
-              <td>${e.age}</td>
-              <td>${e.salary}</td>
-            </tr>
-          `}))}
+          ${this.personsList.map((function(e){return a.dy`
+              <tr>
+                <td>${e.name}</td>
+                <td>${e.age}</td>
+                <td>${e.salary}</td>
+              </tr>
+            `}))}
         </tbody>
       </table>
-      `}})}}]);
+    `}})}}]);
