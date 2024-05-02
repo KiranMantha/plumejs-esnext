f3d26653328c4da5c9528c9d75dbe0966c8a7826
KiranMantha <kiranv.mantha@gmail.com>
2024-05-02 18:11:12 +0530

    added fieldArray functionality

--- src/lib/forms/formBuilder.js
+++ src/lib/forms/formBuilder.js
@@ -1,42 +1,5 @@
-import { signal } from '../core/augment';
-
-const isFunction = (value) => typeof value === 'function';
-
-function nodeName(elem, name) {
-  return elem.nodeName && elem.nodeName.toLowerCase() === name.toLowerCase();
-}
-
-const _getTargetValue = (target) => {
-  let targetValue;
-  switch (target.nodeName && target.nodeName.toLowerCase()) {
-    case 'input':
-    case 'textarea': {
-      let nonTextElements = ['radio', 'checkbox'];
-      if (nonTextElements.includes(target.type)) {
-        targetValue = target.checked ? (target.value !== null && target.value !== 'on' ? target.value : true) : false;
-      } else {
-        targetValue = target.value;
-      }
-      break;
-    }
-    case 'select': {
-      const one = target.type === 'select-one';
-      const options = Array.from(target.options).filter(
-        (option) => !option.disabled && (!option.parentNode.disabled || !nodeName(option.parentNode, 'optgroup'))
-      );
-      const value = [...options]
-        .filter((option) => option.selected)
-        .map((option) => option.value ?? (option.textContent.match(/[^\x20\t\r\n\f]+/g) || []).join(' '));
-      targetValue = one ? value[0] : value;
-      break;
-    }
-    default: {
-      targetValue = target.value;
-      break;
-    }
-  }
-  return targetValue;
-};
+import { FieldArray } from './fieldArray';
+import { FormControl } from './formControl';
 
 export class FormBuilder {
   /**
@@ -54,28 +17,25 @@ export class FormBuilder {
   /**
    * @private
    */
-  _errorCount;
+  _errorCount = 0;
   /**
    * @private
    */
   _isSubmitted = false;
 
   constructor(initialValues) {
-    this._errorCount = signal(0);
     this._initialValues = initialValues;
     for (const [key, value] of Object.entries(initialValues)) {
-      const val = [...(Array.isArray(value) ? value : [value])];
-      this._controls.set(key, {
-        value: val[0],
-        validators: val.length > 1 ? val[1] : [],
-        isTouched: false,
-        errorMessage: ''
-      });
+      if (value instanceof FieldArray) {
+        this._controls.set(key, value);
+      } else {
+        this._controls.set(key, new FormControl(key, value, { setError: this._setError.bind(this) }));
+      }
     }
   }
 
   get hasErrors() {
-    return !!this._errorCount();
+    return !!this._errorCount;
   }
 
   /**
@@ -125,20 +85,7 @@ export class FormBuilder {
    * @returns {{ attrs: { name: string; value: string; onchange: (e: Event) => void; onblur: (e: Event) => void }}
    */
   register(key) {
-    return {
-      attrs: {
-        name: key,
-        value: this.getControl(key).value,
-        onchange: (e) => {
-          const value = _getTargetValue(e.target);
-          this.getControl(key).value = value;
-        },
-        onblur: () => {
-          this.getControl(key).isTouched = true;
-          this._checkValidity(key);
-        }
-      }
-    };
+    return this.getControl(key).register();
   }
 
   /**
@@ -154,56 +101,47 @@ export class FormBuilder {
   }
 
   reset() {
-    for (const [key, value] of Object.entries(this._initialValues)) {
-      const val = [...(Array.isArray(value) ? value : [value])];
-      const { validators } = this._controls.get(key);
-      this._controls.set(key, {
-        value: JSON.parse(JSON.stringify(val))[0],
-        validators,
-        isTouched: false,
-        errorMessage: ''
-      });
-    }
+    this._controls.forEach((ctrl) => {
+      ctrl.reset();
+    });
     this._isSubmitted = false;
     this._errors.clear();
-    this._errorCount.set(0);
+    this._errorCount = 0;
   }
 
   /**
    * @private
    */
-  _checkValidity(controlName) {
-    if (controlName) {
-      this._executeValidators(controlName);
+  _setError(key, validity) {
+    if (validity) {
+      this._errors.set(key, validity);
     } else {
-      this._errors.clear();
-      for (const [key] of this._controls) {
-        this._executeValidators(key);
-      }
+      this._errors.delete(key);
     }
-    this._errorCount.set(this._errors.size);
+    this._errorCount = this._errors.size;
+  }
+
+  /**
+   * @private
+   */
+  _checkValidity() {
+    this._errors.clear();
+    for (const [key] of this._controls) {
+      this._executeValidators(key);
+    }
+    this._errorCount = this._errors.size;
   }
 
   /**
    * @private
    */
   _executeValidators(controlName) {
-    const { value, validators } = this._controls.get(controlName);
-    let errorMessage = '';
-    for (const validator of validators) {
-      const validity = isFunction(validator) ? validator(value) : validator.rule(value);
-      if (validity !== null) {
-        if (this._errors.has(controlName)) {
-          this._errors.set(controlName, { ...this._errors.get(controlName), ...validity });
-        } else {
-          this._errors.set(controlName, validity);
-        }
-        errorMessage = !isFunction(validator) ? validator.message : 'error';
-        break;
-      } else {
-        this._errors.delete(controlName);
-      }
+    const control = this.getControl(controlName);
+    control.validate();
+    if (control.validity !== null) {
+      this._errors.set(controlName, { ...(this._errors.get(controlName) || {}), ...control.validity });
+    } else {
+      this._errors.delete(controlName);
     }
-    this._controls.get(controlName).errorMessage = errorMessage;
   }
 }
